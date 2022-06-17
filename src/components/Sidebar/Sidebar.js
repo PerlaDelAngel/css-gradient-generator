1e938124e16d9a45d39f74c92659bc582bfdab93
Perla Del Ángel <perdelangel@gmail.com>
2022-06-17 09:57:02 -0500

    Adds functionality to direction and color buttons, color randomizer and conversion

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import styles from './Sidebar.module.css';
 import Button from '../Button/Button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import upLeft from '../../assets/Up Left.png';
 import up from '../../assets/Up Arrow.png';
 import upRight from '../../assets/Up Right.png';
@@ -11,13 +11,35 @@ import down from '../../assets/Down Arrow.png';
 import downRight from '../../assets/Down Right.png';
 import circle from '../../assets/Circle.png';
 import Footer from '../Footer/Footer';
+import { randomColor, hexToRGB } from '../../utils/colors';
 
 export default function Sidebar() {
   const [type, setType] = useState('linear');
+  const [direction, setDirection] = useState(); //set default
+  const [color1, setColor1] = useState('');
+  const [color2, setColor2] = useState('');
+
+  const [values, setValues] = useState({
+    gradType: type,
+    gradDirection: direction,
+    firstColor: '',
+    secondColor: ''
+  })
+
+  const randomizeColors = () => {
+    setColor1(randomColor());
+    setColor2(randomColor());
+  };
+
+  useEffect(() => {
+    randomizeColors();
+  }, []);
+
 
   return (
     <section className={styles.sidebar}>
       <h1 className={styles.title}>Css Gradient Generator</h1>
+
 
       <p className={styles.subtitles}>Style</p>
       <div className={styles.type}>
@@ -25,36 +47,89 @@ export default function Sidebar() {
         <Button text='Radial' handleClick={() => setType('radial')} />
       </div>
 
+
       <p className={styles.subtitles}>Direction</p>
       <div className={styles.direction}>
-        <Button img={upLeft} alt='up left arrow' />
-        <Button img={up} alt='top arrow' />
-        <Button img={upRight} alt='up right arrow' />
-        <Button img={left} alt='left arrow' />
-        {type === 'radial' ? <Button img={circle} alt='left arrow' /> : <div/>}
-        <Button img={right} alt='right arrow' />
-        <Button img={downLeft} alt='down left arrow' />
-        <Button img={down} alt='down arrow' />
-        <Button img={downRight} alt='down right arrow' />
+        <Button
+          img={upLeft}
+          alt='up left arrow'
+          handleClick={() => setDirection('to bottom right')} />
+
+        <Button
+          img={up}
+          alt='top arrow'
+          handleClick={() => setDirection('to bottom')} />
+
+        <Button
+          img={upRight}
+          alt='up right arrow'
+          handleClick={() => setDirection('to bottom left')} />
+
+        <Button
+          img={left}
+          alt='left arrow'
+          handleClick={() => setDirection('to right')} />
+
+        {type === 'radial' ?
+          <Button img={circle} alt='left arrow' handleClick={() => setDirection('ellipse at center')} /> :
+          <div />}
+
+        <Button
+          img={right}
+          alt='right arrow'
+          handleClick={() => setDirection('to left')} />
+
+        <Button
+          img={downLeft}
+          alt='down left arrow'
+          handleClick={() => setDirection('to top right')} />
+
+        <Button
+          img={down}
+          alt='down arrow'
+          handleClick={() => setDirection('to top')} />
+
+        <Button
+          img={downRight}
+          alt='down right arrow'
+          handleClick={() => setDirection('to top left')} />
       </div>
+
 
       <p className={styles.subtitles}>Colors</p>
       <div className={styles.colors}>
-        <input type='color' className={styles.color}></input>
-        <input type='color' className={styles.color}></input>
-        <Button text='Random' />
+        <input
+          type='color'
+          className={styles.color}
+          value={color1}
+          onChange={e => setColor1(e.target.value)} />
+
+        <input
+          type='color'
+          className={styles.color}
+          value={color2}
+          onChange={e => setColor2(e.target.value)} />
+
+        <Button text='Random' handleClick={()=> randomizeColors()} />
       </div>
+
 
       <p className={styles.subtitles}>Output format</p>
       <div className={styles.output}>
+        {/* THESE TWO BUTTONS HAVE TO KNOW WHAT KIND OF NUMBER THEY RECEIVE, ie. rgb receives a 
+        string with # they convert to rgb. if hex receives a string starting with (rgb... 
+        convert to hex, THEY HAVE TO RECEIVE BOTH COLORS */}
         <Button text='Hex' />
-        <Button text='Rgba' />
+        <Button text='Rgba' handleClick={() => hexToRGB(color1) }/>
       </div>
 
+
       <button className={`${styles.get} ${styles.getcss}`}>Get CSS</button>
+
       <button className={`${styles.get} ${styles.share}`}>Get Share Link</button>
 
-      <Footer/>
+
+      <Footer />
     </section>
   )
 }
---
+++ src/utils/colors.js
@@ -0,0 +1,31 @@
+export function randomColor() {
+  const randomize = Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+  return '#' + randomize;
+};
+
+//ie. #8C2603
+export function hexToRGB(hex) {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  return "rgba(" + r + ", " + g + ", " + b + ", " + 1 + ")";
+}
+
+//ie. rgba(140, 38, 3, 1.0)
+export function RGBAToHexA(rgba) {
+  const color = rgba.replace(/^rgba?\(|\s+|\)$/g, '').split(',');
+
+  let r = (parseInt(color[0], 10)).toString(16);
+  let g = (parseInt(color[1], 10)).toString(16);
+  let b = (parseInt(color[2], 10)).toString(16);
+
+  if (r.length === 1)
+    r = "0" + r;
+  if (g.length === 1)
+    g = "0" + g;
+  if (b.length === 1)
+    b = "0" + b;
+
+  return ("#" + r + g + b).toUpperCase();
+}

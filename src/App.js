52e8c940c1a63304db9ef670d38a29f197b7071e
Perla Del Ángel <perdelangel@gmail.com>
2022-06-18 09:24:00 -0500

    Added media queries for multiple devices

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState/* , useEffect  */} from 'react';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
